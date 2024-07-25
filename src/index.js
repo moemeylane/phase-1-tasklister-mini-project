064733039df48dc56221cc4345137ecbaf247f67
moemeylane <wangarimaureen668@gmail.com>
2024-07-25 22:33:51 +0300

    complete labs

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const taskForm = document.getElementById("create-task-form");
+  const taskList = document.getElementById("tasks");
+
+  taskForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const taskInput = document.getElementById("new-task-description");
+    const taskDescription = taskInput.value;
+
+    if (taskDescription.trim() === "") {
+      alert("Please enter a task description!");
+      return;
+    }
+
+    const taskItem = document.createElement("li");
+    taskItem.textContent = taskDescription;
+
+    taskList.appendChild(taskItem);
+
+    taskInput.value = "";
+  });
 });
+
